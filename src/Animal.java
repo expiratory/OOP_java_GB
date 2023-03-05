e1bb2fc117b93b62bf1144f5a066c3ea5b22dce8
Aleksandr_Kilchevskiy <alexkill94@mail.ru>
2023-03-05 18:10:44 +0300

    initial commit

---
+++ src/Animal.java
@@ -0,0 +1,15 @@
+public class Animal {
+    private String model;
+
+    public Animal(String model) {
+        this.model = model;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,3 @@
+public class Main {
+    Animal animal = new Animal("");
+}
